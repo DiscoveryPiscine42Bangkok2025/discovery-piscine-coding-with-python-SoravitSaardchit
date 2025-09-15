1b8f58c659f66fd0b6bb179278b44ee4bc7138fb
Soravit_oak <koonkgie@gmail.com>
2025-09-15 20:23:01 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+a = input(" ")
+b = int(a)
+if b > 0:
+    print("This number is positive")
+elif b < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
