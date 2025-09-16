98a383682eadcf31135c6e436825419164907fb3
Soravit_oak <koonkgie@gmail.com>
2025-09-16 09:36:05 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+i = input("Please tell me your age:  ")
+print(f"You are currently {i} years old.")
+k = 10
+for j in range(0,3):
+  print(f"In {k} years, you'll be {int(i)+k} years old")
+  k = k+10
