4e4e075f21fb1fa4ddfb32caca641afe36a49f94
Soravit_oak <koonkgie@gmail.com>
2025-09-17 23:29:11 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,9 +1,11 @@
-a = input(" ")
-if a == " ":
-  print("none")
+a = input().strip()               
+a = a.strip('"\'')              
+if not a:
+    print("none")
 else:
-  ans = input("What was the parameter? ")
-  if a == ans:
-    print("Good job!")
-  else:
-    print("Nope, sorry...")
+    ans = input("What was the parameter? ").strip()
+    ans = ans.strip('"\'')
+    if a == ans:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
