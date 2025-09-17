3fe37fef008aceeac2a38880843a3c645345adee
Soravit_oak <koonkgie@gmail.com>
2025-09-18 01:07:38 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -2,7 +2,7 @@ s = input().strip()
 if not s:
     print("none")
 else:
-    count = sum(1 for ch in s.lower() if ch == 'z')
+    count = s.count('z')   
     if count == 0:
         print("none")
     else:
