ec2f02a686358f264ca949d397747efad9069b4e
Soravit_oak <koonkgie@gmail.com>
2025-09-18 00:49:06 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,7 +1,8 @@
-s = input().strip()        
+s = input().strip()
 if not s:
     print("none")
 else:
     for w in s.split():
+        w = w.strip('"\'.,;:!?')
         if not w.endswith("ism"):
             print(w + "ism")
