f5b2cd0698afab9f16c93c7b9417b4152cc85e00
Soravit_oak <koonkgie@gmail.com>
2025-09-18 11:50:32 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,4 +1,6 @@
 from array import array
-c = array('i', [10,11,50,10,24])
-c_unique = array(c.typecode, set(c))   
-print(c_unique.tolist())
+a = array('i', [2, 8, 9, 48, 8, 22, -12, 2])
+c = array('i', (x + 2 for x in a if x > 5))
+unique = set(c)
+print(a.tolist())  
+print(unique)     
