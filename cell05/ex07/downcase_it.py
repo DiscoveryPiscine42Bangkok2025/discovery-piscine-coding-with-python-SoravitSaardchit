af4deeba20dc38fe170f6e7b4a429ef0c4c385f2
Soravit_oak <koonkgie@gmail.com>
2025-09-18 15:20:53 +0700

    Update downcase_it.py

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -1,7 +1,18 @@
-s = input("")  
-words = s.split()   
-if s == " ":
- print("none")
+import shlex
+def read_line():
+    try:
+        return input().strip()
+    except EOFError:
+        return ""
+line = read_line()
+if not line:
+    print("none")
 else:
-  for a in words:
-    print(a.lower(),end = " ")
+    try:
+        parts = shlex.split(line)
+    except ValueError:
+        parts = line.split()   
+    if not parts:
+        print("none")
+    else:
+        print(parts[0].lower())
